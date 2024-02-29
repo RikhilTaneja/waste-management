2a025d351566ea93904f48d2afae2cd00cbc4da6
Dwij13 <dwijjindal2005@gmail.com>
2024-03-01 00:03:47 +0530

    Created the nav component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='nav-box'>
+    <div className='nav-div'>
+    <h1>Navbar</h1>
+
+    </div>
+
+    </div>
+  )
+}
