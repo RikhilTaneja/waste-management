36b74ed44230000f353634e63940b5f2156e0192
pranshupandey19 <pranshumac19@gmail.com>
2024-03-01 02:49:55 +0530

    login-signup

--- frontend/src/components/Routes/AllRoutes.jsx
+++ frontend/src/components/Routes/AllRoutes.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Home'
 import Signup from '../Signup'
+import SocietyRegister from '../SocietyRegister'
+import Login from '../Login'
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/society/signup' element={<SocietyRegister/>}/>
+      
 
     </Routes>
   )
