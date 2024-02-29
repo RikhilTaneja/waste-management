3a7cd5495c2507d391436f96c493937fb8243224
Dwij13 <dwijjindal2005@gmail.com>
2024-03-01 02:57:27 +0530

    added complaint route and functionality

--- frontend/src/components/Routes/AllRoutes.jsx
+++ frontend/src/components/Routes/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Home'
 import Signup from '../Signup'
+import Complaint from '../Complaint'
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
+      <Route path='/complaint' element={<Complaint/>}/>
 
     </Routes>
   )
