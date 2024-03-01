8faa5460fdeee1d27aae1c8480ccdaec6ef7ebc7
aayushharora <aayushh2k@gmail.com>
2024-03-01 10:58:37 +0530

    PrivateAuth

--- frontend/src/components/Routes/AllRoutes.jsx
+++ frontend/src/components/Routes/AllRoutes.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from '../Home';
-import Signup from '../Signup';
-import Complaint from '../Complaint';
-import SocietyRegister from '../SocietyRegister';
-import Login from '../Login';
-import Donation from '../Donation';
-import Educational from '../Educational';
-import Incentive from '../Incentive';
-import CardDetail from '../CardDetail';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home";
+import Signup from "../Signup";
+import Complaint from "../Complaint";
+import SocietyRegister from "../SocietyRegister";
+import Login from "../Login";
+import Donation from "../Donation";
+import Educational from "../Educational";
+import Incentive from "../Incentive";
+import CardDetail from "../CardDetail";
+import PrivateAuthRoute from "./PrivateAuthRoute";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/complaint' element={<Complaint />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/society/signup' element={<SocietyRegister />} />
-      <Route path='/donation' element={<Donation />} />
-      <Route path='/education' element={<Educational />} />
-      <Route path='/incentive' element={<Incentive />} />
-      <Route path="/incentive/society/:id" element={<CardDetail/>} />
-
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route
+        path="/complaint"
+        element={
+          <PrivateAuthRoute>
+            <Complaint />
+          </PrivateAuthRoute>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/society/signup" element={<SocietyRegister />} />
+      <Route path="/donation" element={<PrivateAuthRoute><Donation /></PrivateAuthRoute>} />
+      <Route path="/education" element={<Educational />} />
+      <Route path="/incentive" element={
+      <PrivateAuthRoute>
+      <Incentive />
+      </PrivateAuthRoute>}/>
+      <Route path="/incentive/society/:id" element={<CardDetail />} />
     </Routes>
   );
 };
